727f267b76f6c07bf940ea8a53dbfc8722ddbd15
prometheus <khalilalkhabasan123@gmail.com>
2021-05-23 12:43:44 +0700

    updating some new addBookHandler

--- src/handler.js
+++ src/handler.js
@@ -2,14 +2,13 @@ const nanoid = require('nanoid');
 const books = require('./books');
 
 //TODO 1 --> addBookHandler
-const addBookHandler = (require, h) => {
+const addBookHandler = (request, h) => {
 	
 	const id = nanoid(16)
 	const {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
-	const finished = books.filter((book) => book.pageCount === book.readPage)
+	const finished = pageCount === readPage;
 	const insertedAt = new Date().toISOString();
 	const updatedAt = insertedAt;
-
 	const newBooks = {
 		name, year, author, summary, publisher, pageCount, readPage, finished, insertedAt, updatedAt
 	}
@@ -25,24 +24,37 @@ const addBookHandler = (require, h) => {
 		response.code(400)
 		return response;
 
-	}
+	};
 	
-	if(readPage === pageCount) {
+	if(readPage > pageCount) {
 		const response = h.response({
 			"status": "fail",
 			"message": "Gagal menambahkan buku, readPage tidak boleh lebih besar dari pageCount",
 		})
 		response.code(400);
 		return response;
+	};
+
+	const isSuccess = books.filter((book) => book.id === id).length > 0;
+	if(isSuccess) {
+		const response = h.response({
+			"status": "success",
+			"message": "Buku berhasil ditambahkan",
+			"data": {
+				"bookId": id,
+			},
+		});
+		response.coed(201);
+		return response;
 	}
 
 	const response = h.response({
-		"status": "success",
-		"message": "Buku berhasil ditambahkan",
-		"data": {
-			"bookId": id,
-		},
+		"status": "fail",
+		"message": "Buku gagal ditambahkan",
 	});
+	response.code(500);
+	return response;
+
 };
 
 //TODO 2 --> getAllBooksHandler
